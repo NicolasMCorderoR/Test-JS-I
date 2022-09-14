129695056537aa9f6be38988e8bdce3bb4d736fe
unknown <nicolascordero.227@gmail.com>
2022-09-14 20:38:43 -0300

    Entrega de la 5ta clase

--- Clase-5-Js/tareas.js
+++ Clase-5-Js/tareas.js
@@ -28,6 +28,7 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+
 }
 
 // No modifiquen nada debajo de esta linea
